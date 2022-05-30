b8ee2302b5c076dbafbd46b8f12294c77410398d
kozincov <kozincov1998@gmail.com>
2022-05-30 13:26:29 +0300

    create template

---
+++ src/pages/Pages.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import {Route, Routes} from "react-router-dom";
+import Test from "./p7-test/Test";
+import Profile from "./p3-profile/Profile";
+import Login from "./p1-login/Login";
+import SignUp from "./p2-signUp/SignUp";
+import NewPassword from "./p4-new-password/NewPassword";
+import ResetPassword from "./p5-reset/ResetPassword";
+import Error404 from "./p6-error/404";
+import s from './Pages.module.css'
+
+export const PATH = {
+    login: 'login',
+    signUp: 'signUp',
+    profile: 'profile',
+    newPassword: 'new-password',
+    resetPassword: 'reset-password',
+    error: '404',
+    test: 'test'
+}
+
+const Pages = () => {
+    return (
+        <div className={s.pagesContainer}>
+            <Routes>
+                <Route path={'/'} element={<Test/>}/>
+                <Route path={PATH.login} element={<Login/>}/>
+                <Route path={PATH.signUp} element={<SignUp/>}/>
+                <Route path={PATH.profile} element={<Profile/>}/>
+                <Route path={PATH.newPassword} element={<NewPassword/>}/>
+                <Route path={PATH.resetPassword} element={<ResetPassword/>}/>
+                <Route path={PATH.error} element={<Error404/>}/>
+                <Route path={PATH.test} element={<Test/>}/>
+                <Route path={'*'} element={<Error404/>}/>
+            </Routes>
+        </div>
+    );
+};
+
+export default Pages;
---
+++ src/pages/p2-signUp/SignUp.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SignUp = () => {
+    return (
+        <div>
+            Sing up a new user
+        </div>
+    );
+};
+
+export default SignUp;
