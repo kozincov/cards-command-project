fb7f7c90b324c9af2927589ac18ac2b82e51f2b4
kozincov <kozincov1998@gmail.com>
2022-05-30 14:10:18 +0300

    update login page

--- src/pages/p1-login/Login.tsx
+++ src/pages/p1-login/Login.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import Input from "../../common/input/Input";
+import Button from "../../common/button/Button";
+import Checkbox from "../../common/checkbox/Checkbox";
 
 const Login = () => {
     return (
-        <div>
-            Log in
-        </div>
+        <>
+            <h1>Log in</h1>
+            <form>
+                <div>
+                    <label htmlFor="username">Username</label>
+                    <Input placeholder="username" name="username" autoComplete="username" required id="username"/>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="password">Password</label>
+                    <Input placeholder="password" name="password" autoComplete="current-password" required id="password"
+                           type="password"/>
+                </div>
+                <Checkbox>Remember me</Checkbox>
+                <Button type="submit">Submit</Button>
+            </form>
+        </>
     );
 };
 
